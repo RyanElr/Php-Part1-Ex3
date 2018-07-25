688e51a0c4ff7aa094dfe20d95fbcaa94ae18ab6
Ryan <ryan.elr@outlook.com>
2018-07-25 14:27:18 +0200

    ex3

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice3 Part1 </title>
+  </head>
+  <body>
+    <?php
+    $km = 1;
+    echo $km.'<br/>';
+    $km = 3;
+    echo $km.'<br/>';
+    $km = 125;
+    echo $km.'<br/>';
+    ?>
+  </body>
+</html>
